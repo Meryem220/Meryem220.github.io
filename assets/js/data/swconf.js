59eeb6eb6d335314e15bfdd19f4ee3d6dce4ac96
Meryem220 <Meryem220@users.noreply.github.com>
2025-08-28 22:45:30 +0000

    deploy: 40781890ec1e645ecc2e5d77f1188eaa35da44bb

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1756421130',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
